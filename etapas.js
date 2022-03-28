f068f6b1f91c83c94d4d540f4a1c15e23a8d9c95
Ivan Rocha <777ivanrocha@gmail.com>
2022-03-28 07:05:07 -0300

    Controle das áreas da tela

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [{
+                    url: '1.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [{
+                    url: '2.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '3.jpg', legenda: 'Prefeito'},
+                    {url: '4,jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: '5.jpeg', legenda: 'Prefeito'},
+                    {url: '6,jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
